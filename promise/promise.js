bd51d66b75aafed5b0c2e9ad58157c2391c5c3ac
김종환 <dondon9513@gmail.com>
2021-09-28 19:02:50 +0900

    try using promiseReject

--- promise/promise.js
+++ promise/promise.js
@@ -1,10 +1,21 @@
-const promiseResolve = new Promise((resolve, reject) => {
+// const promiseResolve = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve("good");
+//   }, 2000);
+// });
+
+// console.log(promiseResolve); // Promise { <pending> }
+// promiseResolve.then((data) =>
+//   console.log(`data : ${data}, promiseResolve : ${promiseResolve}`)
+// ); // 2초뒤 data : good, promiseResolve : [object Promise]
+
+const promiseReject = new Promise((resolve, reject) => {
   setTimeout(() => {
-    resolve("good");
+    reject(new Error("error"));
   }, 2000);
 });
 
-console.log(promiseResolve); // Promise { <pending> }
-promiseResolve.then((data) =>
-  console.log(`data : ${data}, promiseResolve : ${promiseResolve}`)
-); // 2초뒤 data : good, promiseResolve : [object Promise]
+console.log(promiseReject); // Promise { <pending> }
+promiseReject.then((data) =>
+  console.log(`data : ${data}, promiseReject : ${promiseReject}`)
+); // 2초뒤 UnhandledPromiseRejectionWarning: Error: error
