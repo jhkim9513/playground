de6542ed9234b4a5957f37f27ee62c6169fa472c
김종환 <dondon9513@gmail.com>
2021-09-30 19:01:38 +0900

    try using middleware and combineReducers

--- REDUX-Practice/store.js
+++ REDUX-Practice/store.js
@@ -1,20 +1,30 @@
 const redux = require("redux");
 const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
+const reduxLogger = require("redux-logger");
+const logger = reduxLogger.createLogger();
+const combineReducers = redux.combineReducers;
 
 // actions
 // action-types
 const ADD_SUBSCRIBER = "ADD_SUBSCRIBER";
+const ADD_VIEWCOUNT = "ADD_VIEWCOUNT";
 const addSubscriber = () => {
   return {
     type: ADD_SUBSCRIBER,
   };
 };
+const addViewCount = () => {
+  return {
+    type: ADD_VIEWCOUNT,
+  };
+};
 
-// reducers
-const initialState = {
+// reducers : action을 handling함
+const subscriberState = {
   subscribers: 365,
 };
-const reducer = (state = initialState, action) => {
+const subscriberReducer = (state = subscriberState, action) => {
   switch (action.type) {
     case ADD_SUBSCRIBER:
       return {
@@ -26,17 +36,36 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-// store
-const store = createStore(reducer);
+const viewState = {
+  viewCount: 100,
+};
+const viewReducer = (state = viewState, action) => {
+  switch (action.type) {
+    case ADD_VIEWCOUNT:
+      return {
+        ...state,
+        viewCount: state.viewCount + 1,
+      };
+    default:
+      return state;
+  }
+};
 
-//subscribe -view -dispatch
-
-store.subscribe(() => {
-  console.log("subscribe ==>>", store.getState());
+const rootReducer = combineReducers({
+  view: viewReducer,
+  subscriber: subscriberReducer,
 });
 
+// store
+const store = createStore(rootReducer, applyMiddleware(logger));
+
+//subscribe -view -dispatch
+// store.subscribe(() => {
+//   console.log("subscribe ==>>", store.getState());
+// });
+
 store.dispatch(addSubscriber());
 store.dispatch(addSubscriber());
 store.dispatch(addSubscriber());
-store.dispatch(addSubscriber());
-store.dispatch(addSubscriber());
+store.dispatch(addViewCount());
+store.dispatch(addViewCount());
