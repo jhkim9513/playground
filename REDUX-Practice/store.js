9731dfcfa160307fcd079635979108ea784e09c1
김종환 <dondon9513@gmail.com>
2021-09-30 18:41:00 +0900

    start learning Redux

---
+++ REDUX-Practice/store.js
@@ -0,0 +1,42 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+// actions
+// action-types
+const ADD_SUBSCRIBER = "ADD_SUBSCRIBER";
+const addSubscriber = () => {
+  return {
+    type: ADD_SUBSCRIBER,
+  };
+};
+
+// reducers
+const initialState = {
+  subscribers: 365,
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_SUBSCRIBER:
+      return {
+        ...state,
+        subscribers: state.subscribers + 1,
+      };
+    default:
+      return state;
+  }
+};
+
+// store
+const store = createStore(reducer);
+
+//subscribe -view -dispatch
+
+store.subscribe(() => {
+  console.log("subscribe ==>>", store.getState());
+});
+
+store.dispatch(addSubscriber());
+store.dispatch(addSubscriber());
+store.dispatch(addSubscriber());
+store.dispatch(addSubscriber());
+store.dispatch(addSubscriber());
