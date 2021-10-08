3c298eb1d9ddb158af818f09ceeac1754657e958
김종환 <dondon9513@gmail.com>
2021-10-08 13:00:06 +0900

    try using thunk

--- react_redux/src/components/Comments.js
+++ react_redux/src/components/Comments.js
@@ -2,12 +2,24 @@ import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { fetchComments } from "../redux/index";
 
-function Comments({ fetchComments }) {
+function Comments({ fetchComments, loading, comments }) {
   useEffect(() => {
     fetchComments();
   }, [fetchComments]);
 
-  return <div className="items"></div>;
+  const commentsItems = loading ? (
+    <div>is loading...</div>
+  ) : (
+    comments.map((comment) => (
+      <div key={comment.id}>
+        <h3>{comment.name}</h3>
+        <p>{comment.email}</p>
+        <p>{comment.body}</p>
+      </div>
+    ))
+  );
+
+  return <div className="comments">{commentsItems}</div>;
 }
 
 const mapStateToProps = ({ comments }) => {
--- react_redux/src/redux/comments/actions.js
+++ react_redux/src/redux/comments/actions.js
@@ -5,11 +5,32 @@ import {
   FETCH_COMMENTS_FAILURE,
 } from "./types";
 
+export const fetchCommentSuccess = (comments) => {
+  return {
+    type: FETCH_COMMENTS_SUCCESS,
+    payload: comments,
+  };
+};
+
+export const fetchCommentFailure = (err) => {
+  return {
+    type: FETCH_COMMENTS_FAILURE,
+    payload: err,
+  };
+};
+
+export const fetchCommentRequest = () => {
+  return {
+    type: FETCH_COMMENTS_REQUEST,
+  };
+};
+
 export const fetchComments = () => {
   return (dispatch) => {
+    dispatch(fetchCommentRequest());
     fetch("https://jsonplaceholder.typicode.com/comments")
       .then((response) => response.json())
-      .then((comments) => console.log(comments))
-      .catch(console.log);
+      .then((comments) => dispatch(fetchCommentSuccess(comments)))
+      .catch((err) => dispatch(fetchCommentFailure(err)));
   };
 };
